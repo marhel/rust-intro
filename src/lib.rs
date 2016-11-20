d9196338585d6d0293d190198e54464d437ff6ba
Martin Hellspong <martin.hellspong@factor10.com>
2016-11-20 12:19:52 +0100

    Rename InterruptPriorityEncoder to InterruptController

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,27 @@
 pub mod constants;
 
 struct Motherboard<'a> {
-    ipe: InterruptPriorityEncoder<'a>,
+    int_ctrl: InterruptController<'a>,
     core: Core,
 }
 
 struct Core {
     mask: u8,
-    vector: u8
+    vector: Option<u8>
 }
 
 impl Core {
-    fn process_interrupt(&mut self, ipe: &mut InterruptPriorityEncoder) {
-        let prio = ipe.highest_priority;
-        if prio > self.mask {            
-            self.vector = ipe.acknowledge_interrupt(prio);
-        }        
+    fn process_interrupt(&mut self, int_ctrl: &mut InterruptController) {
+        let prio = int_ctrl.highest_priority;
+        self.vector = if prio > self.mask {            
+            Some(int_ctrl.acknowledge_interrupt(prio))
+        } else {
+            None
+        }
     }
 }
 
-struct InterruptPriorityEncoder<'a>
+struct InterruptController<'a>
 {
     highest_priority: u8,
     asserted: [Option<&'a Peripheral>; 7]
@@ -29,7 +31,7 @@ fn priority_to_index(priority: u8) -> usize {
     7 - priority as usize
 }
 
-impl<'a> InterruptPriorityEncoder<'a> {
+impl<'a> InterruptController<'a> {
     fn update_asserted(&mut self, index: usize, value: Option<&'a Peripheral>) -> u8 {
         self.asserted[index] = value;
         self.highest_priority = self.asserted.iter().position(|&x| x.is_some()).map(|i| 7-i as u8).unwrap_or(0u8);
@@ -59,36 +61,38 @@ struct Peripheral
 
 #[cfg(test)]
 mod tests {
-    use super::{Motherboard, Core, InterruptPriorityEncoder, Peripheral};
+    use super::{Motherboard, Core, InterruptController, Peripheral};
     #[test]
     fn highest_priority_is_processed_first() {
         let rtc = Peripheral { priority : 7, vector: 12 };
         let disk = Peripheral { priority : 5, vector: 15 };
         let keyboard = Peripheral { priority : 2, vector: 17 };
 
-        let ipe = InterruptPriorityEncoder {
+        let int_ctrl = InterruptController {
             highest_priority: 0, 
             asserted: [None, None, None, None, None, None, None]
         };
-        let core = Core { mask: 0, vector: 0 };
-        let mut board = Motherboard { ipe: ipe, core: core };
+        let core = Core { mask: 0, vector: None };
+        let mut board = Motherboard { int_ctrl: int_ctrl, core: core };
 
-        board.ipe.assert_interrupt(&rtc);
-        board.ipe.assert_interrupt(&keyboard);
-        board.ipe.assert_interrupt(&disk);
+        board.int_ctrl.assert_interrupt(&rtc);
+        board.int_ctrl.assert_interrupt(&keyboard);
+        board.int_ctrl.assert_interrupt(&disk);
 
-        assert_eq!(7, board.ipe.highest_priority);
-        board.core.process_interrupt(&mut board.ipe);
-        assert_eq!(12, board.core.vector);
+        assert_eq!(7, board.int_ctrl.highest_priority);
+        board.core.process_interrupt(&mut board.int_ctrl);
+        assert_eq!(Some(12), board.core.vector);
 
-        assert_eq!(5, board.ipe.highest_priority);
-        board.core.process_interrupt(&mut board.ipe);
-        assert_eq!(15, board.core.vector);
+        assert_eq!(5, board.int_ctrl.highest_priority);
+        board.core.process_interrupt(&mut board.int_ctrl);
+        assert_eq!(Some(15), board.core.vector);
 
-        assert_eq!(2, board.ipe.highest_priority);
-        board.core.process_interrupt(&mut board.ipe);
-        assert_eq!(17, board.core.vector);
+        assert_eq!(2, board.int_ctrl.highest_priority);
+        board.core.process_interrupt(&mut board.int_ctrl);
+        assert_eq!(Some(17), board.core.vector);
 
-        assert_eq!(0, board.ipe.highest_priority);
+        assert_eq!(0, board.int_ctrl.highest_priority);        
+        board.core.process_interrupt(&mut board.int_ctrl);
+        assert_eq!(None, board.core.vector);
     }
 }
