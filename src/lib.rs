6d0b790707f2c9a0ccde04a5a1609d6af6bc8db9
Martin Hellspong <martin.hellspong@factor10.com>
2016-11-20 12:42:08 +0100

    Turn InterruptController into a trait w/one impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub mod constants;
 
 struct Motherboard<'a> {
-    int_ctrl: InterruptController<'a>,
+    int_ctrl: &'a mut InterruptController,
     core: Core,
 }
 
@@ -12,7 +12,7 @@ struct Core {
 
 impl Core {
     fn process_interrupt(&mut self, int_ctrl: &mut InterruptController) {
-        let prio = int_ctrl.highest_priority;
+        let prio = int_ctrl.highest_priority();
         self.vector = if prio > self.mask {            
             Some(int_ctrl.acknowledge_interrupt(prio))
         } else {
@@ -20,8 +20,13 @@ impl Core {
         }
     }
 }
+trait InterruptController
+{
+    fn highest_priority(&self) -> u8;
+    fn acknowledge_interrupt(&mut self, priority: u8) -> u8;
+}
 
-struct InterruptController<'a>
+struct VectoredInterruptController<'a>
 {
     highest_priority: u8,
     asserted: [Option<&'a Peripheral>; 7]
@@ -30,8 +35,7 @@ struct InterruptController<'a>
 fn priority_to_index(priority: u8) -> usize {
     7 - priority as usize
 }
-
-impl<'a> InterruptController<'a> {
+impl<'a> VectoredInterruptController<'a> {
     fn update_asserted(&mut self, index: usize, value: Option<&'a Peripheral>) -> u8 {
         self.asserted[index] = value;
         self.highest_priority = self.asserted.iter().position(|&x| x.is_some()).map(|i| 7-i as u8).unwrap_or(0u8);
@@ -41,6 +45,13 @@ impl<'a> InterruptController<'a> {
     {
         self.update_asserted(priority_to_index(p.priority), Some(p))
     }
+}
+
+impl<'a> InterruptController for VectoredInterruptController<'a> {
+    fn highest_priority(&self) -> u8 {
+        self.highest_priority
+    }
+
     fn acknowledge_interrupt(&mut self, priority: u8) -> u8 {
         let ip = priority_to_index(priority);
         match self.asserted[ip] {
@@ -61,38 +72,37 @@ struct Peripheral
 
 #[cfg(test)]
 mod tests {
-    use super::{Motherboard, Core, InterruptController, Peripheral};
+    use super::{Motherboard, Core, InterruptController, VectoredInterruptController, Peripheral};
     #[test]
     fn highest_priority_is_processed_first() {
         let rtc = Peripheral { priority : 7, vector: 12 };
         let disk = Peripheral { priority : 5, vector: 15 };
         let keyboard = Peripheral { priority : 2, vector: 17 };
 
-        let int_ctrl = InterruptController {
+        let mut int_ctrl = VectoredInterruptController {
             highest_priority: 0, 
             asserted: [None, None, None, None, None, None, None]
         };
+        int_ctrl.assert_interrupt(&rtc);
+        int_ctrl.assert_interrupt(&keyboard);
+        int_ctrl.assert_interrupt(&disk);
         let core = Core { mask: 0, vector: None };
-        let mut board = Motherboard { int_ctrl: int_ctrl, core: core };
+        let mut board = Motherboard { int_ctrl: &mut int_ctrl, core: core };
 
-        board.int_ctrl.assert_interrupt(&rtc);
-        board.int_ctrl.assert_interrupt(&keyboard);
-        board.int_ctrl.assert_interrupt(&disk);
-
-        assert_eq!(7, board.int_ctrl.highest_priority);
-        board.core.process_interrupt(&mut board.int_ctrl);
+        assert_eq!(7, board.int_ctrl.highest_priority());
+        board.core.process_interrupt(board.int_ctrl);
         assert_eq!(Some(12), board.core.vector);
 
-        assert_eq!(5, board.int_ctrl.highest_priority);
-        board.core.process_interrupt(&mut board.int_ctrl);
+        assert_eq!(5, board.int_ctrl.highest_priority());
+        board.core.process_interrupt(board.int_ctrl);
         assert_eq!(Some(15), board.core.vector);
 
-        assert_eq!(2, board.int_ctrl.highest_priority);
-        board.core.process_interrupt(&mut board.int_ctrl);
+        assert_eq!(2, board.int_ctrl.highest_priority());
+        board.core.process_interrupt(board.int_ctrl);
         assert_eq!(Some(17), board.core.vector);
 
-        assert_eq!(0, board.int_ctrl.highest_priority);        
-        board.core.process_interrupt(&mut board.int_ctrl);
+        assert_eq!(0, board.int_ctrl.highest_priority());        
+        board.core.process_interrupt(board.int_ctrl);
         assert_eq!(None, board.core.vector);
     }
 }
